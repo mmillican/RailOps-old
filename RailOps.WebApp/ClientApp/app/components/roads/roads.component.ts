e4ecfaa9315f15a141062c6b3dd79011171845f0
Matt Millican <matt@mattmillican.com>
2017-08-31 23:59:14 -0500

    Add success/error message for roads component and better form handling

--- RailOps.WebApp/ClientApp/app/components/roads/roads.component.ts
+++ RailOps.WebApp/ClientApp/app/components/roads/roads.component.ts
@@ -9,6 +9,11 @@ import { Road } from '../../models/roster/road';
     templateUrl: './roads.component.html'
 })
 export class RoadsComponent implements OnInit {
+    public loading: boolean = false;
+
+    public error: string;
+    public success: string;
+
     public roads: Road[];
 
     public editingRoad: Road = new Road();
@@ -20,18 +25,41 @@ export class RoadsComponent implements OnInit {
     }
 
     saveRoad() {
+        this.loading = true;
+        this.clearMessages();
+        
         this.roadService.createRoad(this.editingRoad)
             .subscribe(result => {
                 if (result) {
-                    this.refreshRoads();
+                    this.roads.push(result);
+
+                    this.loading = false;
+                    this.success = 'The road has been created';
                 }
+            }, error => {
+                this.error = 'Error creating new road';
+                this.loading = false;
             });
     }
 
     private refreshRoads() {
+        this.clearMessages();
         this.roadService.getRoads()
             .subscribe(result => {
                 this.roads = result;
+            }, error => {
+                this.error = 'There was an error retrieving the roads.'
             });
+    }
+
+    private clearMessages() {
+        this.error = '';
+        this.success = '';
+    }
+
+    private resetForm() {
+        this.editingRoad.id = 0;
+        this.editingRoad.name = '';
+        this.editingRoad.code = '';
     }
 }
